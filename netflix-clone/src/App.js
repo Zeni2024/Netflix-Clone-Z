e4eb2133588e16d78b04de59abcd785b25d8304c
Zeni2024 <newera2117@gmail.com>
2024-09-06 22:44:58 +0300

    Header and Footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Heelloo</h1>
+      {/* <h1>Heelloo</h1> */}
+      <Home />
     </div>
   );
 }
